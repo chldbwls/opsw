fffcfd20b132d73a3cd5fcf6f71f0bfa29af37e0
chldbwls <chldbwls8643@naver.com>
2023-04-02 22:58:43 +0900

    4주차 과제2

---
+++ week4_2/week4/week4_4/app/src/main/java/com/example/week4_4/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.week4_4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    //2021041084최유진
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
